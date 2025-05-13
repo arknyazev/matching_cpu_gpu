ad8a81a6f5666897c64ba943671585e8ba310f8d
Alex Knyazev <alexkniazev@gmail.com>
2025-05-13 15:11:11 -0400

    Added compartison scripts.

--- count_lossess.py
+++ count_lossess.py
@@ -1,9 +1,14 @@
 import numpy as np
-timelost = np.loadtxt('output/timelost.txt')
-tmax = 1e-3
-count = 0
-for t in timelost:
-    count += (t < tmax)
 
-print(f'Lost particle {count=}')
-print(f'This is {count/len(timelost)} of all partickes')
+def count_losses(timelost : np.ndarray, tmax: float = 1e-3) -> None:
+    count = 0
+    for t in timelost:
+        count += (t < tmax)
+    print(f'Lost particle {count=}')
+    print(f'This is {count/len(timelost)} of all partickes')
+
+print('\n=== CPU: ===\n')
+count_losses(np.loadtxt('output/timelost.txt'))
+
+print('\n=== GPU: ===\n')
+count_losses(np.loadtxt('gpu_all_loss_times.txt'))
